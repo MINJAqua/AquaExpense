342517448418e8fa6ebee3e6e5b9ab86f94219d1
Apples <junyiappleli@gmail.com>
2022-09-08 11:23:08 -0400

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/expense", require("./routes/expenseRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});

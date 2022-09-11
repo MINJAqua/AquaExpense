70cbd576b96c5e3f1ba5f26db789e7efb72a70b0
Stacey Ali <96086935+Slam256@users.noreply.github.com>
2022-09-11 15:54:18 -0400

    Connect

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import "../css/Signup.css";
+import '../css/Signup.css'
 import {
   Grid,
   Paper,
@@ -9,73 +9,65 @@ import {
   Button,
   Typography,
   Link,
-} from "@mui/material";
-import LockIcon from "@mui/icons-material/Lock";
-import InfoIcon from "@mui/icons-material/Info";
-import { useRef, useState, useEffect } from "react";
-import axios from "../axios";
+} from '@mui/material'
+import LockIcon from '@mui/icons-material/Lock'
+import { useRef, useState, useEffect } from 'react'
+import axios from '../axios'
 
-const USER_REGEX = /^[A-z][A-z]$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const SIGNUP_URL = "/api/users";
+const USER_REGEX = /^[A-z][A-z]$/
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const SIGNUP_URL = '/api/users'
 
 const SignUp = () => {
   const paperStyle = {
     padding: 20,
-    height: "70vh",
+    height: '70vh',
     width: 300,
-    margin: "20px auto",
-  };
+    margin: '20px auto',
+  }
 
   const avatarStyle = {
-    backgroundColor: "green",
-  };
+    backgroundColor: 'green',
+  }
 
-  const userRef = useRef();
-  const errRef = useRef();
+  const userRef = useRef()
+  const errRef = useRef()
 
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [validName, setValidName] = useState(false);
-  const [userFocus, setUserFocus] = useState(false);
+  const [user, setUser] = useState('')
+  const [email, setEmail] = useState('')
+  const [validName, setValidName] = useState(false)
 
-  const [password, setPassword] = useState("");
-  const [validPwd, setValidPwd] = useState(false);
-  const [pwdFocus, setPwdFocus] = useState(false);
+  const [password, setPassword] = useState('')
+  const [validPwd, setValidPwd] = useState(false)
 
-  const [matchPwd, setMatchPwd] = useState("");
-  const [validMatch, setValidMatch] = useState(false);
-  const [matchFocus, setMatchFocus] = useState(false);
+  const [matchPwd, setMatchPwd] = useState('')
+  const [validMatch, setValidMatch] = useState(false)
 
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
 
   useEffect(() => {
-    userRef.current.focus();
-  }, []);
+    const result = USER_REGEX.test(user)
+    console.log(result)
+    console.log(user)
+    setValidName(result)
+  }, [user])
 
   useEffect(() => {
-    const result = USER_REGEX.test(user);
-    console.log(result);
-    console.log(user);
-    setValidName(result);
-  }, [user]);
+    const result = PWD_REGEX.test(password)
+    console.log(result)
+    console.log(password)
+    setValidPwd(result)
+    const match = password === matchPwd
+    setValidMatch(match)
+  }, [password, matchPwd])
 
   useEffect(() => {
-    const result = PWD_REGEX.test(password);
-    console.log(result);
-    console.log(password);
-    setValidPwd(result);
-    const match = password === matchPwd;
-    setValidMatch(match);
-  }, [password, matchPwd]);
-
-  useEffect(() => {
-    setErrMsg("");
-  }, [user, password, matchPwd]);
+    setErrMsg('')
+  }, [user, email, password, matchPwd])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // if button enabled with JS hack
     // const v1 = USER_REGEX.test(user);
@@ -84,26 +76,35 @@ const SignUp = () => {
     //   setErrMsg("Invalid Entry");
     //   return;
     // }
-    console.log("hello");
 
     try {
       const response = await axios.post(
         SIGNUP_URL,
-        JSON.stringify({ user, email, password }),
+        JSON.stringify({ name: user, email, password }),
         {
-          headers: { "Content-Type": "application/json" },
+          headers: { 'Content-Type': 'application/json' },
           withCredentials: true,
         }
-      );
-
-      console.log(response);
-    } catch (error) {
-      console.log(error);
+      )
+      console.log('hello')
+      console.log(response?.data)
+      console.log(response?.accessToken)
+      console.log(JSON.stringify(response))
+      setSuccess(true)
+      //clear state and controlled inputs
+      //need value attrib on inputs for this
+      setUser('')
+      setPassword('')
+      setEmail('')
+      setMatchPwd('')
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response')
+      } else {
+        setErrMsg('Registration Failed')
+      }
     }
-
-    console.log(user, password);
-    setSuccess(true);
-  };
+  }
   return (
     <>
       {success ? (
@@ -111,120 +112,102 @@ const SignUp = () => {
           <h1>Success!</h1>
           <p>
             {/* React router link */}
-            <Link href="#"> HomePage</Link>
+            <Link href='#'> HomePage</Link>
           </p>
         </section>
       ) : (
         <Grid>
           <Paper elevation={10} style={paperStyle}>
-            <Grid align="center">
+            <Grid align='center'>
               <Avatar style={avatarStyle}>
                 <LockIcon />
               </Avatar>
-              <p
-                ref={errRef}
-                className={errMsg ? "errmsg" : "offscreen"}
-                aria-live="assertive"
-              >
+              <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
                 {errMsg}
               </p>
               <h2>Sign UP</h2>
             </Grid>
 
             <TextField
-              id="name"
+              id='name'
               ref={userRef}
               onChange={(e) => setUser(e.target.value)}
-              aria-invalid={validName ? "false" : "true"}
-              aria-describedby="uidnote"
-              onFocus={() => setUserFocus(true)}
-              onBlur={() => setUserFocus(false)}
               fullWidth
-              label="Name"
-              htmlFor="name"
-              placeholder="Enter Name"
+              label='Name'
+              htmlFor='name'
+              placeholder='Enter Name'
               required
-              autoComplete="off"
+              autoComplete='off'
             />
             <TextField
-              style={{ margin: "20px 0px 0px" }}
-              id="email"
+              style={{ margin: '20px 0px 0px' }}
+              id='email'
               onChange={(e) => setEmail(e.target.value)}
               fullWidth
-              label="Email"
-              htmlFor="email"
-              placeholder="Enter email"
+              label='Email'
+              htmlFor='email'
+              placeholder='Enter email'
               required
-              autoComplete="off"
+              autoComplete='off'
             />
 
             <TextField
-              style={{ margin: "20px 0px" }}
-              type="password"
-              id="password"
+              style={{ margin: '20px 0px' }}
+              type='password'
+              id='password'
               onChange={(e) => setPassword(e.target.value)}
-              aria-invalid={validPwd ? "false" : "true"}
-              aria-describedby="pwdnote"
-              onFocus={() => setPwdFocus(true)}
-              onBlur={() => setPwdFocus(false)}
               fullWidth
-              label="Password"
-              htmlFor="password"
-              placeholder="Enter password"
+              label='Password'
+              htmlFor='password'
+              placeholder='Enter password'
               required
             />
-            <p
-              id="pwdnote"
-              className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+            {/* <p
+              id='pwdnote'
+              className={!validPwd ? 'instructions' : 'offscreen'}
             >
-              <InfoIcon fontSize="small" />
+              <InfoIcon fontSize='small' />
               8 to 24 characters.
               <br />
               Must include uppercase and lowercase letters, a number and a
               special character.
               <br />
-              Allowed special characters:{" "}
-              <span aria-label="exclamation mark">!</span>{" "}
-              <span aria-label="at symbol">@</span>{" "}
-              <span aria-label="hashtag">#</span>{" "}
-              <span aria-label="dollar sign">$</span>{" "}
-              <span aria-label="percent">%</span>
-            </p>
+              Allowed special characters:{' '}
+              <span aria-label='exclamation mark'>!</span>{' '}
+              <span aria-label='at symbol'>@</span>{' '}
+              <span aria-label='hashtag'>#</span>{' '}
+              <span aria-label='dollar sign'>$</span>{' '}
+              <span aria-label='percent'>%</span>
+            </p> */}
             <TextField
-              type="password"
-              id="confirm_pwd"
+              type='password'
+              id='confirm_pwd'
               onChange={(e) => setMatchPwd(e.target.value)}
-              aria-invalid={validPwd ? "false" : "true"}
-              aria-describedby="confirmnote"
-              onFocus={() => setMatchFocus(true)}
-              onBlur={() => setMatchFocus(false)}
               fullWidth
-              label="Confirm Password"
-              htmlFor="confirm_pwd"
-              placeholder="Confirm Password"
+              label='Confirm Password'
+              htmlFor='confirm_pwd'
+              placeholder='Confirm Password'
               required
             />
-            <p
-              id="confirmnote"
-              className={
-                matchFocus && !validMatch ? "instructions" : "offscreen"
-              }
+            {/* <p
+              id='confirmnote'
+              className={!validMatch ? 'instructions' : 'offscreen'}
             >
-              <InfoIcon fontSize="small" />
+              <InfoIcon fontSize='small' />
               Must match the first password input field.
-            </p>
+            </p> */}
             <FormControlLabel
-              control={<Checkbox name="checkedB" color="primary" />}
-              label="Remember Me"
+              control={<Checkbox name='checkedB' color='primary' />}
+              label='Remember Me'
             />
 
             <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              component="label"
+              type='submit'
+              color='primary'
+              variant='contained'
+              component='label'
               fullWidth
-              style={{ margin: "8px 0" }}
+              style={{ margin: '8px 0' }}
               //disabled={!validName || !validPwd || !validMatch ? true : false}
               onClick={handleSubmit}
             >
@@ -233,13 +216,13 @@ const SignUp = () => {
 
             <Typography>
               Do you have already have an account
-              <Link href="#"> Sign-In</Link>
+              <Link href='#'> Sign-In</Link>
               {/* React router link */}
             </Typography>
           </Paper>
         </Grid>
       )}
     </>
-  );
-};
-export default SignUp;
+  )
+}
+export default SignUp

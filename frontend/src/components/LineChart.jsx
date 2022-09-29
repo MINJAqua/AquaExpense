d3ad76ea3a08324bc69a6c82c4b393850eb3686c
Max Zhao <maxzhao98@gmail.com>
2022-09-28 20:01:01 -0500

    LineChart CSS

--- frontend/src/components/LineChart.jsx
+++ frontend/src/components/LineChart.jsx
@@ -43,19 +43,19 @@ const LineGraph = ({ transactions, account }) => {
   }
 
   return (
-    <div className='lineChart-container'>
-      <AreaChart width={730} height={250} data={fillData()} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+    <div className='lineChart'>
+      <div className='lineChart-title'>Cost of Transactions by Date</div>
+      <AreaChart width={730} height={250} data={fillData()}>
         <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+          <linearGradient id="amount" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#72efff" stopOpacity={0.5}/>
+            <stop offset="95%" stopColor="#72efff" stopOpacity={.1}/>
           </linearGradient>
         </defs>
-        <XAxis dataKey="date" />
-        <YAxis dataKey='amount'/>
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Area type="monotone" dataKey="amount" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" dot={{ stroke: 'purple', strokeWidth: 2 }}/>
+        <XAxis dataKey="date" stroke='gray'/>
+        <CartesianGrid strokeDasharray="3 3" className='chartGrid'/>
+        <Tooltip itemStyle={{color: '#36c9fe'}}/>
+        <Area type="monotone" dataKey="amount" stroke="#72efff" fillOpacity={1} fill="url(#amount)" dot={{ stroke: '#36c9fe', strokeWidth: 2 }}/>
       </AreaChart>
     </div>
   )

7bde9bc062157e1dc020991f96749ee3b356d4d1
Apples <junyiappleli@gmail.com>
2022-10-09 10:02:53 -0400

    testing

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
@@ -21,6 +22,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/expense", require("./routes/expenseRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/plaid", require("./routes/plaid"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get(
+    "*",
+    (req, res) => res,
+    sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 

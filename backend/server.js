a8e288f5618704c314f7fd7377eff159903a153b
Apples <junyiappleli@gmail.com>
2022-10-17 17:06:41 -0400

    make it better

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use("/api/plaid", require("./routes/plaid"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get(
-    "*",
-    (req, res) => res,
-    sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
